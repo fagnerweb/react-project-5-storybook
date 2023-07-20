08286f50fbdf574cad661cd9304d28b2b29a4bf6
Fagner Silva <fagnermarketing2014@gmail.com>
2023-07-20 10:46:32 -0300

    finish

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Surface/Box',
+  component: Box,
+  tags: ['autodocs'],
+  args: {
+    children: <Text>Testando o elemento Box</Text>,
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
